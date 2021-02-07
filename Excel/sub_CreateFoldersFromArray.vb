524538b85cda4995739680c2ee7c6e444e6ed489
Alexander Barbone, PE <78570406+abarbone@users.noreply.github.com>
2021-02-07 13:21:41 -0500

    Updated structure
    Updated the structure to clarify procedure.

--- Excel/sub_CreateFoldersFromArray.vb
+++ Excel/sub_CreateFoldersFromArray.vb
@@ -1,30 +1,42 @@
 Sub CreateFoldersFromArray(ByRef FolderArray() As String)
 ' Creates folders based on a 1 dimensional array of folder paths.
-' Commit date: 2021-02-05
+' Commit date: 2021-02-07
 
-On Error GoTo ErrorCatch
-ErrorMessage = "An unknown error has occurred."
+'0.0. Setup.
 
-' Declare variables
-Dim i As Long
-Dim Parent As String
+    '0.1. Set error behavior.
 
-' Loop through array
-For i = LBound(FolderArray) To UBound(FolderArray)
+    On Error GoTo ErrorCatch
+    ErrorMessage = "An unknown error has occurred."
+
+    '0.2. Declare variables
     
-    ' Check that the Parent Folder can be created.
-    Parent = ParentFolder(FolderArray(i))
-    If Dir(Parent, vbDirectory) = "" Then
-        ErrorMessage = "Cannot find parent folder: " & Parent _
-        & vbCrLf & vbCrLf & "Operation aborted."
-        GoTo ErrorCatch
-    End If
+    Dim i As Long
+    Dim Parent As String
+
+'1.0. Check inputs.
+
+    'Not used.
+
+'2.0. Loop through array
+    For i = LBound(FolderArray) To UBound(FolderArray)
     
-    ' Check if folder already exists. If not, create it.
-    If Dir(FolderArray(i), vbDirectory) = "" Then
-        MkDir FolderArray(i)
-    End If
-Next i
+        ' Check that the Parent Folder can be created.
+        Parent = ParentFolder(FolderArray(i))
+        If Dir(Parent, vbDirectory) = "" Then
+            ErrorMessage = "Cannot find parent folder: " & Parent _
+            & vbCrLf & vbCrLf & "Operation aborted."
+            GoTo ErrorCatch
+        End If
+    
+        ' Check if folder already exists. If not, create it.
+        If Dir(FolderArray(i), vbDirectory) = "" Then
+            MkDir FolderArray(i)
+        End If
+
+    Next i
+
+'3.0. Close out.
 
 Exit Sub
 
